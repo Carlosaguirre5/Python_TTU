20a16bce23061387b68642d4782a29a352ba65cc
Carlos Aguirre <carlosaguirre@Carloss-MacBook-Air.local>
2024-11-12 08:58:58 -0600

    My first commit

---
+++ quicktraining_prior_course.py
@@ -0,0 +1,29 @@
+'''## Exercise 1: Python Basics
+
+# Sample solution
+a = 10
+b = 5
+
+# Operations
+print("Addition:", a + b)
+print("Substraction:", a - b)
+print("Multiplication", a * b)
+print("Division:", a / b)
+
+'''
+# Square function
+def square(x):
+        return x * x
+#print("Square of 4:", square(4))
+
+## Exercise 2: Lits and Loops
+# Sample solutions 
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    print("Square:", square(number))
+
+## Exercise 3: Strings - Lenght and Uppercase
+
+name = "Carlos"
+print("Lenght of name:", len(name))
+print("Name in uppercase:", name.upper())
